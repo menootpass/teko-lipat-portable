c644d3521faf5163d2981843863017e7a928b938
MNPass <mrs.farhanary@gmail.com>
2024-08-06 16:28:12 +0700

    second update

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import GopayIcon from "../components/svg/gopay.svg";
 import DanaIcon from "../components/svg/dana.svg";
 import BriIcon from "../components/svg/bri.svg";
 import FlipIcon from "../components/svg/flip.svg";
-
+import { namaProduk, Deskripsi, spesifikasi, harga, waUrl } from "../lib/datas";
 export default function Home() {
   return (
     <main>
@@ -29,39 +29,22 @@ export default function Home() {
               ></Image>
             </div>
             <div class="md:w-1/2 md:pl-6 mt-4 md:mt-0">
-              <h1 class="text-2xl font-bold">Teko Lipat Portable</h1>
+              <h1 class="text-2xl font-bold">{namaProduk}</h1>
               <div class="mt-2">
-                <span class="text-gray-500 line-through">Rp. 290.000</span>
-                <span class="text-red-500 text-2xl font-bold">Rp. 149.900</span>
+                <span class="text-gray-500 line-through">{harga[0]}</span>
+                <span class="text-red-500 text-2xl font-bold">{harga[1]}</span>
               </div>
-              <p class="mt-4 text-gray-700">
-                Apakah anda sering bepergian dan butuh solusi praktis untuk
-                memasak air? Teko Lipat Elektrik ini adalah jawabannya! Dengan
-                desain yang inovatif dan fungsionalitas yang luar biasa, Anda
-                bisa memasak air kapan saja dan di mana saja tanpa ribet.
-              </p>
-              <p class="mt-4 text-gray-700">
-                {/* <b className="text-2xl font-extrabold">Manfaat Utama:</b> <br /> */}
-                <b>Portabel & Simpel: </b>Teko ini bisa dilipat, sehingga mudah
-                dibawa ke mana-mana. Tidak perlu khawatir lagi mencari tempat
-                untuk memasak air saat bepergian. <br />
-                <b>Mudah Digunakan:</b> Hanya dengan satu kali pencet, Anda bisa
-                memasak air dengan cepat dan efisien.
-                <br />
-                <b>Desain Modern:</b> Tampil dengan desain yang elegan dan
-                minimalis, cocok untuk digunakan di rumah, kantor, atau saat
-                traveling.
-                <br />
-                <b>Hemat Energi:</b> Konsumsi daya yang efisien membuat teko ini
-                tidak hanya praktis tetapi juga ekonomis.
-                <br />
-                <br />
-                <b className="text-red-500">Dapatkan harga termurah</b>
-              </p>
+              <p class="mt-4 text-gray-700">{Deskripsi}</p>
+              <p
+                class="mt-4 text-gray-700"
+                dangerouslySetInnerHTML={{ __html: spesifikasi }}
+              ></p>
+              <b className="text-red-500">Dapatkan harga termurah</b>
               <br />
               <br />
               <a
-                href="https://wa.me/6289606883082?text=Permisi,%20aku%20tertarik%20untuk%20membeli%20Teko%20Lipat%20Elektrik%20dengan%20harga%20Rp.%20149.900,%20apakah%20stoknya%20masih%20ada?"
+                target="_blank"
+                href={waUrl}
                 class="mt-4 bg-blue-700 text-white py-2 px-8 rounded-full hover:bg-blue-600"
               >
                 BELI
